29e0692217e38bc0dc2df9fb55134a96d30a41b7
rodrigodepool <rodrigodp05@gmail.com>
2018-12-06 15:36:12 +0100

    Auto-pushed

--- algorithms/uva/10142.cpp
+++ algorithms/uva/10142.cpp
@@ -9,16 +9,42 @@
 using namespace std;
 typedef tuple<int, int, int> tupla;
 
-map <string, int> cands;
+map <int, int> cands;
 vector<string>candsnames;
 void resolver(vector<queue<int>> entrada){
     do{
-        int min=0;
-        int max=0;
-        vector<string> mincands;
-        for(queue<int> q: entrada){
-            // TODO contar bla bla bla
-        }
+        //TODO MAL
+    //     int minv=0;
+    //     vector<int> minc;
+    //     int maxv=0;
+    //     vector<int> maxc;
+    //     int total=0;
+    //     for(auto& val:cands){
+    //         total++;
+    //         if(val.second>maxv){
+    //             maxc.clear();
+    //             maxc.push_back(val.first);
+    //         }else if(val.second>=maxv){
+    //             maxc.push_back(val.first);
+    //         }
+    //         if(val.second<minv){
+    //             minc.clear();
+    //             minc.push_back(val.first);
+    //         }else if(val.second<=minv){
+    //             minc.push_back(val.first);
+    //         }
+    //     }
+
+    //     if(minv==maxv){
+    //         for(auto& num:maxc) cout<<candsnames[num]<<endl;
+    //         return;
+    //     }
+    //     if(double(maxc.size())/double(total) > 0.5){
+
+    //     }
+    //     for(queue<int> q: entrada){
+    //         // TODO contar bla bla bla
+    //     }
     }while();
 
     return;
@@ -40,7 +66,7 @@ int main(){
         candsnames.resize(n);
         for(int j=0; j<n; j++){
             getline(cin, candsnames[j]);
-            cands[candsnames[j]]=0;
+            cands[j]=0;
             // cout<< j << cands[j] << endl;
         }
 
@@ -55,8 +81,11 @@ int main(){
         while(line.length()>0){
             istringstream iss(line);
             string item;
+            int numi=0;
             while(getline(iss, item, ' ')){
+                if(numi==0) cands[stoi(item)]++;
                 entradas[numvotos].push(stoi(item));
+                numi++;
             }
             if(cin){
                 getline(cin, line);
@@ -66,7 +95,6 @@ int main(){
             numvotos++;
         }
 
-        
         resolver(entradas);
     }
     return 0;
