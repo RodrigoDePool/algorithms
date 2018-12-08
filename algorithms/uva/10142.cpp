fe625f3f78747be0ebbf056afe15d6664588b1bc
rodrigodepool <rodrigodp05@gmail.com>
2018-12-08 12:06:19 +0100

    Auto-pushed

--- algorithms/uva/10142.cpp
+++ algorithms/uva/10142.cpp
@@ -10,58 +10,57 @@
 using namespace std;
 typedef tuple<int, int, int> tupla;
 
-int minaux;
-int maxv;
 int maxcand;
-set<int> mincands;
 unordered_map<int, int> contarVotos(set<int> cands, vector<queue<int>>entrada, int size){
     unordered_map<int, int> votes;
     // Conteo de votos, ademas saca los maxvs y minvs
     // Deja en maxcand uno de los maximos  
     for(set<int>::iterator ptr=cands.begin(); ptr!=cands.end(); ptr++)
         votes[*ptr]=0;
-    maxv=-1;
+    
+    maxcand=*cands.begin();
     for(int i=0;i<size;i++){
         queue<int> q = entrada[i];
         while(cands.count(q.front())!=1)
             q.pop();
         votes[q.front()]++;
-        if(votes[q.front()]>maxv){
-            maxv=votes[q.front()];
+        if(votes[q.front()]>votes[maxcand])
             maxcand=q.front();
-        }
-        if(i==0 || votes[q.front()]<votes[minaux]){
-            minaux=q.front();
-            mincands.clear();
-        }
-        if(votes[q.front()]==votes[minaux])
-            mincands.insert(i);
     }
     return votes;
 }
 
 void resolver(set<int> cands, vector<string> candsnames, vector<queue<int>> entrada, int size){
     unordered_map<int, int> votes = contarVotos(cands, entrada, size);
-    int total=size;
-    if(maxv == votes[minaux]){
+    
+    int mincand = *cands.begin();
+    set<int> mincands = {*cands.begin()};
+    for(set<int>::iterator ptr=cands.begin(); ptr!=cands.end(); ptr++){
+        if(votes[*ptr]<votes[mincand]){
+            mincand=*ptr;
+            mincands.clear();
+        }
+        if(votes[*ptr]==votes[mincand])
+            mincands.insert(*ptr);
+    }
+    
+    if(votes[maxcand] == votes[mincand]){
         /*Empate, imprimios todos*/
         for(set<int>:: iterator ptr=cands.begin(); ptr!=cands.end(); ptr++)
             cout << candsnames[*ptr] << endl;
         return;
     }
-    if(double(maxv)/total > 0.5){
+
+    if(double(votes[maxcand])/size > 0.5){
         /*Gana el maximo*/
         cout<<candsnames[maxcand]<<endl;
         return;
     }
+
     // No hay ganador, eliminamos a los minimos
-    for(int i:mincands){
-        queue<int> q = entrada[i];
-        // Eliminamos el candidato de los votos y la lista de cands
-        cands.erase(q.front());
-        q.pop();
-    }
-    mincands.clear();
+    for(int i:mincands)
+        cands.erase(i);
+    
     return resolver(cands, candsnames, entrada, size);
 }
 
--- makefile
+++ makefile
@@ -9,8 +9,7 @@ UVA := 10196 10142 10033 10137
 
 all: $(GRAPH) $(UVA)
 	clear
-	./10142 <data2
-	# ./10137 < data
+	./10137 < data
 	
 %: $(GRAPH_DIR)/%.cpp
 	g++ $(FLAGS) -o $@ $^
