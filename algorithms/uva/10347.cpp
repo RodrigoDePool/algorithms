453ac1fc4a735fa377a57256d3c7a6f024a9ad67
rodrigodepool <rodrigodp05@gmail.com>
2018-12-10 23:36:20 +0100

    Auto-pushed

---
+++ algorithms/uva/10347.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <cstdio>
+using namespace std;
+int main(){
+    int ma, mb, mc;
+    // DA WRONG ANSWER!!!!
+    double ar, a;
+
+    while(cin >> ma >> mb >> mc){
+        a = (ma + mb + mc +0.0)/2;
+        if(a-ma<=0 || a-mb<=0 || a-mc<=0){
+            cout << -1 << endl;
+        }else{
+            ar = 4*(sqrt(a*(a-ma)*(a-mb)*(a-mc)))/3;
+            int sol=round(ar*1000);
+            printf("%d.%03d\n",sol/1000,sol%1000);
+        }
+    }
+
+
+
+}
--- makefile
+++ makefile
@@ -3,7 +3,7 @@ FLAGS := -g -lm -lcrypt  -pipe -DONLINE_JUDGE
 GRAPH_DIR := ./algorithms/graphs
 UVA_DIR := ./algorithms/uva
 
-EXE := dfs bfs 10196 10142 10033 10137 10107
+EXE := dfs bfs 10196 10142 10033 10137 10107 10347
 
 
 all: $(EXE)
