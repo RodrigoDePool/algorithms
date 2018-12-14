0cf1997f134daf0d69c76d74e43e776c7557187a
rodrigodepool <rodrigodp05@gmail.com>
2018-12-14 12:01:18 +0100

    Auto-pushed

---
+++ algorithms/uva/11495.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+int bsort(vector<int> a, int min, int max, acumm){
+
+
+}
+
+
+int main(){
+    int first = true;
+    while(true){
+        int n;
+        cin >> n;
+        if(n==0) break;
+        if(first)
+            first = false;
+        else
+            cout << endl;
+
+        vector<int> seq;
+        for(int ii=0; i<n;i++){
+            int aux;
+            cin>>aux;
+            seq.push_back(aux-1);
+        
+        }
+        int res = bsort(aux,-1,aux.size(),0);
+    }
+
+}
