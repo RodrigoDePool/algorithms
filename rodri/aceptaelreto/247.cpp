9ae669f0865c115aace0767b6dd871d651e1f63d
rodrigodepool <rodrigodp05@gmail.com>
2019-02-18 20:32:54 +0100

    msg

--- rodri/aceptaelreto/247.cpp
+++ rodri/aceptaelreto/247.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(){
-    int n;
-    bool sol;
-    cin >> n;
-    while(n!=0){
-        int prev, curr;
-        prev=-1; 
-        sol=true;
-        for(int i=0; i<n; i++){
-            cin >> curr;
-            if(curr<=prev)
-                sol=false;
-            prev=curr;
-        }
-        if(sol)
-            cout << "SI" << endl;
-        else
-            cout << "NO" << endl;
-        cin >> n;
+int main() {
+  int n;
+  bool sol;
+  cin >> n;
+  while (n != 0) {
+    int prev, curr;
+    prev = -1;
+    sol = true;
+    for (int i = 0; i < n; i++) {
+      cin >> curr;
+      if (curr <= prev)
+        sol = false;
+      prev = curr;
     }
-    return 0;
+    if (sol)
+      cout << "SI" << endl;
+    else
+      cout << "NO" << endl;
+    cin >> n;
+  }
+  return 0;
 }
--- rodri/uva/10359.py
+++ rodri/uva/10359.py
@@ -1,10 +1,10 @@
 sols = [1, 3]
-for i in range(2,250):
-    sols.append(sols[i-1]+sols[i-2]*2)
+for i in range(2, 250):
+    sols.append(sols[i - 1] + sols[i - 2] * 2)
 try:
-    while(True):
+    while (True):
         num = int(input())
         if num == 0: print(1)
-        else: print(sols[num-1])
+        else: print(sols[num - 1])
 except:
     pass
